27973380150d647b1ff47bc97a57a595f35ba054
Mori from 42 <64525549+angelidito@users.noreply.github.com>
2023-06-03 01:02:56 +0200

    All the includes for the allowed functions

--- minishell.h
+++ minishell.h
@@ -1,10 +1,23 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
+# include <curses.h>            // tgetent, tgetflag, tgetnum, tgetstr, tgoto,
+								// tputs
+# include <dirent.h>            // opendir, readdir, closedir
+# include <errno.h>             // perror
+# include <fcntl.h>             // open
+# include <readline/history.h>  // add_history
+# include <readline/readline.h> // readline, rl_clear_history, rl_on_new_line,
+								// rl_replace_line, rl_redisplay
+# include <signal.h>            // sigaction, sigemptyset, sigaddset, kill
+# include <stdio.h>             // printf
+# include <stdlib.h>            // malloc, free, exit
+# include <string.h>            // strerror
+# include <sys/stat.h>          // stat, lstat, fstat
+# include <sys/types.h>         // wait, waitpid, wait3, wait4, signal, getpid,
+								// getppid
+# include <termios.h>           // isatty, tcsetattr, tcgetattr
+# include <unistd.h>            // write, access, fork, execve, close, pipe,
+								//  dup, dup2, getpid, getppid, getcwd, chdir
 
 #endif
